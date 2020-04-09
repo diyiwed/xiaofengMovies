f43b67fc4bc169868cd7a9a6a1e6a96748ee36cb
叮当猫 <5447221+dingding_mao@user.noreply.gitee.com>
2020-04-09 15:30:33 +0800

    4月9日上午

---
+++ src/routers/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
+import adminRouter from './admin'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  // base: process.env.BASE_URL,
+  base: 'xiaofeng',
+  routes: [
+    movieRouter,
+    mineRouter,
+    cinemaRouter,
+    adminRouter,
+    {
+      path: '/',
+      redirect: '/movie'
+    }
+  ]
+})
+
